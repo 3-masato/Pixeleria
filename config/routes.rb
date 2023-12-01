cd30906542fdad6dca8abbe5bf96852d7a65edde
Masato <144527087+3-masato@users.noreply.github.com>
2023-12-01 08:27:53 +0000

    Add: deviseで管理者と一般ユーザーを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  get 'homes/top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # 管理者
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # 一般ユーザー
+  devise_for :user, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
 end
