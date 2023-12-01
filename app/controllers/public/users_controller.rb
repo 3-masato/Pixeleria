e669f529b0dfca3f6319d18811facdf482e4dda1
Masato <144527087+3-masato@users.noreply.github.com>
2023-12-01 12:54:03 +0000

    Add: Usersコントローラーとルーティング設定

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,16 @@
+class Public::UsersController < ApplicationController
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def confirm_deactivation
+  end
+
+  def deactivate
+  end
+end
