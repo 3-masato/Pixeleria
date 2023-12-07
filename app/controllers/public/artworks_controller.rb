3022bf525e5f07c0537ddbd428c30cc6a05d601c
Masato <144527087+3-masato@users.noreply.github.com>
2023-12-07 08:30:56 +0000

    Update: サイズ変更機能を実装

--- app/controllers/public/artworks_controller.rb
+++ app/controllers/public/artworks_controller.rb
@@ -1,6 +1,8 @@
 class Public::ArtworksController < ApplicationController
   before_action :authenticate_user!
 
+  BASE_CANVAS_SIZE = 768
+
   def index
   end
 
@@ -12,6 +14,13 @@ class Public::ArtworksController < ApplicationController
 
   def new
     @artwork = Artwork.new
+  end
+
+  def initialize_editor
+    size = params[:size].split("x")
+    @width = size[0].to_i
+    @height = size[1].to_i
+    @dotsize = BASE_CANVAS_SIZE / @width
   end
 
   def update
--- app/helpers/public/artworks_helper.rb
+++ app/helpers/public/artworks_helper.rb
@@ -1,2 +1,11 @@
 module Public::ArtworksHelper
+  def artwork_size_options
+    options_for_select([
+      ['Extra Small (16x16)', '16x16'],
+      ['Small (32x32)', '32x32'],
+      ['Medium (64x64)', '64x64'],
+      ['Large (128x128)', '128x128'],
+      ['Extra Large (256x256)', '256x256'],
+    ])
+  end
 end
