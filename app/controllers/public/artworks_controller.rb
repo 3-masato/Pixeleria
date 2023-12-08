aaf6a0e2f48a59e2ebdb9e69f7da4db0270d3a25
Masato <144527087+3-masato@users.noreply.github.com>
2023-12-08 09:41:09 +0000

    Update: 作品投稿機能仮実装

--- app/controllers/public/artworks_controller.rb
+++ app/controllers/public/artworks_controller.rb
@@ -1,12 +1,16 @@
 class Public::ArtworksController < ApplicationController
   before_action :authenticate_user!
 
+  # ドット絵のサイズいずれでも割り切れるきりの良い数
+  # 256x256 のドット絵なら 1ドットが3px
   BASE_CANVAS_SIZE = 768
 
   def index
   end
 
   def show
+    @artwork = Artwork.find(params[:id])
+    @author = @artwork.user
   end
 
   def edit
@@ -20,7 +24,8 @@ class Public::ArtworksController < ApplicationController
     size = params[:size].split("x")
     @width = size[0].to_i
     @height = size[1].to_i
-    @dotsize = BASE_CANVAS_SIZE / @width
+    dot_ratio = [@width, @height].max
+    @dotsize = BASE_CANVAS_SIZE / dot_ratio
   end
 
   def update
@@ -30,6 +35,22 @@ class Public::ArtworksController < ApplicationController
     data = params[:artwork]
     width = params[:width]
     height = params[:height]
+
+    @artwork = Artwork.new(
+      :title => "test",
+      :is_public => true,
+      :pixel_data => data,
+      :width => width,
+      :height => height,
+      :user_id => current_user.id
+    )
+
+    if @artwork.save
+      redirect_to artwork_path(@artwork), notice: "saved"
+    else
+      # TODO
+      # render :new
+    end
   end
 
   def destroy
