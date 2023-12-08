69397261524ad8babb6a2d41c403c1e3ef8c3542
Masato <144527087+3-masato@users.noreply.github.com>
2023-12-08 10:39:13 +0000

    Update: トップ画面に最近投稿された作品を表示

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,8 @@
 class Public::HomesController < ApplicationController
+  DISPLAY_NEW_ARTWORKS_COUNT = 12
+
   def top
+    @artworks = Artwork.limit(DISPLAY_NEW_ARTWORKS_COUNT)
   end
 
   def about
