1ae85a34edbdb3630f2f627f4d0e30246fb5a6be
Masato <144527087+3-masato@users.noreply.github.com>
2023-12-11 02:47:26 +0000

    Fix: コメント投稿/削除時のN+1対策

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,6 +1,6 @@
 class Public::CommentsController < ApplicationController
   before_action :ensure_correct_user, only: :destroy
-  before_action :set_artwork, only: %i[create destroy]
+  before_action :set_artwork_and_comments, only: %i[create destroy]
 
   def create
     comment = current_user.comments.new(comment_params)
@@ -14,8 +14,9 @@ class Public::CommentsController < ApplicationController
   end
 
   private
-  def set_artwork
+  def set_artwork_and_comments
     @artwork = Artwork.find(params[:artwork_id])
+    @artwork_comments = @artwork.comments.with_user_profile_images
   end
 
   def comment_params
