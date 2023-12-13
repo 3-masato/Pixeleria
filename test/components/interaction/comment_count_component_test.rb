a88bc8669fa6516f50423fad1d81741e4d6b9d9b
Masato <144527087+3-masato@users.noreply.github.com>
2023-12-13 14:15:43 +0000

    Add: 投稿作品のコメント数表示機能を実装

---
+++ test/components/interaction/comment_count_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Interaction::CommentCountComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Interaction::CommentCountComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
