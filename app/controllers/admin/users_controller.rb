10f353d0375c4f483caa5a1ea257a2a721ca6fdb
3-masato <144527087+3-masato@users.noreply.github.com>
2023-12-19 06:17:41 +0000

    Update: 管理者側のユーザー編集機能実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,36 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
+
   def index
     @users = User.page(params[:page])
   end
 
   def show
-    @user = User.find_by(account_name: params[:account_name])
     @user_artworks = @user.artworks.with_details.page(params[:page])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user.account_name), notice: t("messages.user.profile.update_success")
+    else
+      render :edit
+    end
+  end
+
+  private
+  def set_user
+    @user = User.find_by(account_name: params[:account_name])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :display_name,
+      :account_name,
+      :introduction,
+      :profile_image
+    )
   end
 end
