95e295de4e2118ebe37378556c4947f64a0a3af3
3-masato <144527087+3-masato@users.noreply.github.com>
2023-12-20 04:26:58 +0000

    Add: いいねカウントコンポーネントを追加

---
+++ test/components/interaction/like_count_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class Interaction::LikeCountComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Interaction::LikeCountComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
