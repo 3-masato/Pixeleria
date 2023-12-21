2c07fd1963308412902478867b10ddb5372556d9
3-masato <144527087+3-masato@users.noreply.github.com>
2023-12-21 15:09:12 +0000

    Update: ユーザー側の全体的なレイアウト調整

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -15,10 +15,14 @@ module.exports = {
       },
     },
   },
+  variants: {
+    imageRendering: ["responsive"],
+  },
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
     require("@tailwindcss/typography"),
     require("@tailwindcss/container-queries"),
+    require("tailwindcss-image-rendering")(),
   ]
 };
