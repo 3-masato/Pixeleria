1b78a8891d86899f80a6477086e3d83459e021b0
3-masato <144527087+3-masato@users.noreply.github.com>
2024-01-16 13:05:26 +0000

    Update: ディレクトリ移動

--- test/components/shared/details_row_component_test.rb
+++ test/components/shared/details_row_component_test.rb
@@ -2,11 +2,11 @@
 
 require "test_helper"
 
-class Artwork::DetailsRowComponentTest < ViewComponent::TestCase
+class Shared::DetailsRowComponentTest < ViewComponent::TestCase
   def test_component_renders_something_useful
     # assert_equal(
     #   %(<span>Hello, components!</span>),
-    #   render_inline(Artwork::DetailsRowComponent.new(message: "Hello, components!")).css("span").to_html
+    #   render_inline(Shared::DetailsRowComponent.new(message: "Hello, components!")).css("span").to_html
     # )
   end
 end
