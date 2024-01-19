d8cdf73bdfa853059bca0ec293133817a06ce297
3-masato <144527087+3-masato@users.noreply.github.com>
2024-01-19 11:30:29 +0000

    Update: jQuery削除

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -8,7 +8,6 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 import "controllers";
-// import "jquery";
 import "./dist/pixeleria-editor";
 
 Rails.start();
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,12 +5,4 @@ const webpack = require("webpack");
 const typescript = require("./loaders/typescript");
 environment.loaders.prepend("typescript", typescript);
 
-// environment.plugins.prepend(
-//   "Provide",
-//   new webpack.ProvidePlugin({
-//     $: "jquery/src/jquery",
-//     jQuery: "jquery/src/jquery",
-//   })
-// );
-
 module.exports = environment;
